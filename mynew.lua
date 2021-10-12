af5eeeb2a6cb12df93bdb688dcde546b23a2f76e
ValeriiVavilin <85596671+ValeriiVavilin@users.noreply.github.com>
2021-10-13 00:51:23 +0200

    Add files via upload

---
+++ mynew.lua
@@ -0,0 +1,85 @@
+script_author("Alkoigel")
+
+require "lib.moonloader" -- подключение библиотеки
+local dlstatus = require('moonloader').download_statuslocal
+local keys = require "vkeys"
+local imgui = require 'imgui'
+local encoding = require 'encoding'
+local se = require 'lib.samp.events'
+local ids = -1
+encoding.default = 'CP1251'
+u8 = encoding.UTF8
+
+local main_color = 0x5A90CE
+local main_color_text = "{5A90CE}"
+local white_color = "{FFFFFF}"
+
+latest = "0.0.1"
+script_version ("0.0.1")
+
+function main()
+    if not isSampLoaded() or not isSampfuncsLoaded() then return end
+    while not isSampAvailable() do wait(100) end
+
+		while true do
+			wait (0)
+			autoupdate("тут ссылка на джейсон", '['..string.upper(thisScript().name)..']: ', "vk.com/alkoigel")
+		end
+end
+
+
+function autoupdate(json_url, prefix, url) -- -- Author: http://qrlk.me/samp
+  local dlstatus = require('moonloader').download_status
+  local json = getWorkingDirectory() .. '\\'..thisScript().name..'-version.json'
+  if doesFileExist(json) then os.remove(json) end
+  downloadUrlToFile(json_url, json,
+    function(id, status, p1, p2)
+      if status == dlstatus.STATUSEX_ENDDOWNLOAD then
+        if doesFileExist(json) then
+          local f = io.open(json, 'r')
+          if f then
+            local info = decodeJson(f:read('*a'))
+            updatelink = info.updateurl
+            updateversion = info.latest
+            f:close()
+            os.remove(json)
+            if updateversion ~= thisScript().version then
+              lua_thread.create(function(prefix)
+                local dlstatus = require('moonloader').download_status
+                local color = -1
+                sampAddChatMessage(('{0000FF}[Ghelper]: {FFFFFF}Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion), color)
+                wait(250)
+                downloadUrlToFile(updatelink, thisScript().path,
+                  function(id3, status1, p13, p23)
+                    if status1 == dlstatus.STATUS_DOWNLOADINGDATA then
+                      print(string.format('Загружено %d из %d.', p13, p23))
+                    elseif status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
+                      print('Загрузка обновления завершена.')
+                      sampAddChatMessage(('{0000FF}[Ghelper]: {FFFFFF}Обновление завершено!'), color)
+                      goupdatestatus = true
+                      lua_thread.create(function() wait(500) thisScript():reload() end)
+                    end
+                    if status1 == dlstatus.STATUSEX_ENDDOWNLOAD then
+                      if goupdatestatus == nil then
+                        sampAddChatMessage(('{0000FF}[Ghelper]: {FFFFFF}Обновление прошло неудачно. Запускаю устаревшую версию..'), color)
+                        update = false
+                      end
+                    end
+                  end
+                )
+                end, prefix
+              )
+            else
+              update = false
+              print('v'..thisScript().version..': Обновление не требуется.')
+            end
+          end
+        else
+          print('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..url)
+          update = false
+        end
+      end
+    end
+  )
+  while update ~= false do wait(100) end
+end
